47099c4f52dc8253a23a6fd797dc6819fee4e96d
ViscaDwipayanti <viscadwipayanti05@gmail.com>
2025-09-16 13:29:57 +0800

    portfolio dengan tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,js}"],
+   theme: {
+   container: {
+     center: true,
+     padding: '16px'
+   },
+     extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        primary: '#0891b2',
+        secondary: '#64748b',
+        dark: '#0f172a'
+      },
+      screens: {
+        '2xl': '1320px'
+      }
+     },
+   },
+   plugins: [],
+ }
