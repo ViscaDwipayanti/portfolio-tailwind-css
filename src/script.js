5575a0dfc3c98b725d0a98d8417c63651f9f4bb0
ViscaDwipayanti <viscadwipayanti05@gmail.com>
2025-09-17 13:33:17 +0800

    Menambahkan fitur darkMode dan perbaikan fitur lainnya

--- src/script.js
+++ src/script.js
@@ -1,22 +1,54 @@
 // Navbar Fixed
- window.onscroll = function() {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
-    if(window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-    } else{
-        header.classList.remove('navbar-fixed');
-    }
- }
-
-
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
 
 // hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu')
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function(){
-    hamburger.classList.toggle('hamburger-active')
-    navMenu.classList.toggle('hidden')
-})
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+//klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+//Darkmode
+const darktoggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+// Saat halaman dimuat, cek localStorage
+if (localStorage.getItem("theme") === "dark") {
+  html.classList.add("dark");
+  darktoggle.checked = true;
+}
+
+// Saat toggle diklik, ubah tema dan simpan ke localStorage
+darktoggle.addEventListener("click", function () {
+  if (darktoggle.checked) {
+    html.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    html.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  }
+});
